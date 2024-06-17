e4a7c3eb94db0ff2a6db5a3f242ada2f7d95da36
Wtgonw <57482600+Wtgonw@users.noreply.github.com>
2024-06-17 13:36:19 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -11,18 +11,21 @@ from tqdm import tqdm
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 parser = argparse.ArgumentParser(description="PyTorch BasicIRSTD Inference without mask")
-parser.add_argument("--model_names", default=['ACM', 'ALCNet','DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+',  
-                    help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
-parser.add_argument("--pth_dirs", default=None, nargs='+',  help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+# parser.add_argument("--model_names", default=['ACM', 'ALCNet', 'DNANet', 'ISNet', 'RDIAN', 'ISTDU-Net'], nargs='+', help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet'")
+# parser.add_argument("--model_names", default=['SCTransNet'], nargs='+', help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet', 'SCTransNet'")
+# parser.add_argument("--model_names", default=['ISTDU-Net'], nargs='+', help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet', 'SCTransNet'")
+parser.add_argument("--model_names", default=['UIUNet', 'ISTDU-Net'], nargs='+', help="model_name: 'ACM', 'ALCNet', 'DNANet', 'ISNet', 'UIUNet', 'RDIAN', 'ISTDU-Net', 'U-Net', 'RISTDnet', 'SCTransNet'")
+# parser.add_argument("--pth_dirs", default=None, nargs='+', help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+# parser.add_argument("--pth_dirs", default=['WideIRSTD/SCTransNet_1.pth.tar'], nargs='+', help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+# parser.add_argument("--pth_dirs", default=['WideIRSTD/ISTDU-Net_150.pth.tar'], nargs='+', help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+# parser.add_argument("--pth_dirs", default=['WideIRSTD/UIUNet_300.pth.tar', 'WideIRSTD/ISTDU-Net_150.pth.tar'], nargs='+', help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
+parser.add_argument("--pth_dirs", default=['WideIRSTD/UIUNet_300.pth.tar', 'WideIRSTD/ISTDU-Net_150.pth.tar'], nargs='+', help="checkpoint dir, default=None or ['NUDT-SIRST/ACM_400.pth.tar','NUAA-SIRST/ACM_400.pth.tar']")
 parser.add_argument("--dataset_dir", default='./datasets', type=str, help="train_dataset_dir")
-parser.add_argument("--dataset_names", default=['NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+', 
-                    help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea'")
-parser.add_argument("--img_norm_cfg", default=None, type=dict,
-                    help="specific a img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
-parser.add_argument("--img_norm_cfg_mean", default=None, type=float,
-                    help="specific a mean value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
-parser.add_argument("--img_norm_cfg_std", default=None, type=float,
-                    help="specific a std value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+# parser.add_argument("--dataset_names", default=['NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K'], nargs='+', help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea'")
+parser.add_argument("--dataset_names", default=['WideIRSTD'], nargs='+', help="dataset_name: 'NUAA-SIRST', 'NUDT-SIRST', 'IRSTD-1K', 'SIRST3', 'NUDT-SIRST-Sea'")
+parser.add_argument("--img_norm_cfg", default=None, type=dict, help="specific a img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+parser.add_argument("--img_norm_cfg_mean", default=None, type=float, help="specific a mean value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
+parser.add_argument("--img_norm_cfg_std", default=None, type=float, help="specific a std value img_norm_cfg, default=None (using img_norm_cfg values of each dataset)")
 
 parser.add_argument("--save_img", default=True, type=bool, help="save image of or not")
 parser.add_argument("--save_img_dir", type=str, default='./results/', help="path of saved image")
@@ -33,15 +36,15 @@ global opt
 opt = parser.parse_args()
 ## Set img_norm_cfg
 if opt.img_norm_cfg_mean != None and opt.img_norm_cfg_std != None:
-  opt.img_norm_cfg = dict()
-  opt.img_norm_cfg['mean'] = opt.img_norm_cfg_mean
-  opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
-  
-def test():
+    opt.img_norm_cfg = dict()
+    opt.img_norm_cfg['mean'] = opt.img_norm_cfg_mean
+    opt.img_norm_cfg['std'] = opt.img_norm_cfg_std
+
+def test(model_name):
     test_set = InferenceSetLoader(opt.dataset_dir, opt.train_dataset_name, opt.test_dataset_name, opt.img_norm_cfg)
     test_loader = DataLoader(dataset=test_set, num_workers=1, batch_size=1, shuffle=False)
 
-    net = Net(model_name=opt.model_name, mode='test').cuda()
+    net = Net(model_name=model_name, mode='test').cuda()
     try:
         net.load_state_dict(torch.load(opt.pth_dir)['state_dict'])
     except:
@@ -49,40 +52,43 @@ def test():
         net.load_state_dict(torch.load(opt.pth_dir, map_location=device)['state_dict'])
     net.eval()
 
-    # 固定分割的大小
     with torch.no_grad():
         for idx_iter, (img, size, img_dir) in tqdm(enumerate(test_loader)):
             img = Variable(img).cuda()
-            if size[0] <= 2048 and size[1] <= 2048:
-                pred = net.forward(img)
-                pred = pred[:, :, :size[0], :size[1]]
+            if model_name == 'ISTDU-Net':
+                if size[0] <= 2048 and size[1] <= 2048:
+                    pred = net.forward(img)
+                    pred = pred[:, :, :size[0], :size[1]]
+                else:
+                    continue
             else:
-                pred_rows = []
-                split_size = 2048
-                for i in range(0, size[0], split_size):
-                    pred_cols = []
-                    for j in range(0, size[1], split_size):
-                        end_i = min(i + split_size, size[0])
-                        end_j = min(j + split_size, size[1])
-                        part_img = img[:, :, i:end_i, j:end_j]
-                        pred = net.forward(part_img)
-                        pred_cols.append(pred)
-                    pred_combined = torch.cat(pred_cols, dim=3)
-                    pred_rows.append(pred_combined)
-
-                pred = torch.cat(pred_rows, dim=2)
-                pred = pred[:, :, :size[0], :size[1]]
+                if size[0] <= 2048 and size[1] <= 2048:
+                    continue
+                else:
+                    pred_rows = []
+                    split_size = 2048
+                    for i in range(0, size[0], split_size):
+                        pred_cols = []
+                        for j in range(0, size[1], split_size):
+                            end_i = min(i + split_size, size[0])
+                            end_j = min(j + split_size, size[1])
+                            part_img = img[:, :, i:end_i, j:end_j]
+                            pred = net.forward(part_img)
+                            pred_cols.append(pred)
+                        pred_combined = torch.cat(pred_cols, dim=3)
+                        pred_rows.append(pred_combined)
+    
+                    pred = torch.cat(pred_rows, dim=2)
+                    pred = pred[:, :, :size[0], :size[1]]
 
             ### save img
             if opt.save_img == True:
                 img_save = transforms.ToPILImage()(((pred[0, 0, :, :] > opt.threshold).float()).cpu())
-                if not os.path.exists(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name):
-                    os.makedirs(opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name)
-                img_save.save(
-                    opt.save_img_dir + opt.test_dataset_name + '/' + opt.model_name + '/' + img_dir[0] + '.png')
+                if not os.path.exists(opt.save_img_dir + opt.test_dataset_name + '/UIUNet'):
+                    os.makedirs(opt.save_img_dir + opt.test_dataset_name + '/UIUnet')
+                img_save.save(opt.save_img_dir + opt.test_dataset_name + '/UIUNet/' + img_dir[0] + '.png')
 
     print('Inference Done!')
-
 
 if __name__ == '__main__':
     opt.f = open(opt.save_log + 'test_' + (time.ctime()).replace(' ', '_').replace(':', '_') + '.txt', 'w')
@@ -115,7 +121,7 @@ if __name__ == '__main__':
                         print(opt.test_dataset_name)
                         opt.f.write(opt.test_dataset_name + '\n')
                         opt.pth_dir = opt.save_log + pth_dir
-                        test()
+                        test(model_name)
                         print('\n')
                         opt.f.write('\n')
         opt.f.close()
